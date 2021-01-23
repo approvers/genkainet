bab79035716981d50ecb4c9347d99e85fb31a611
Shuntaro Nishizawa <me@shun.technology>
2021-01-24 02:11:18 +0900

    feat: add console styles

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,13 +1,15 @@
 import React, { FC } from "react";
 import { Message } from "../shared";
 
+import styles from "./Messages.module.scss";
+
 type Props = {
   messages: Message[];
   myId: string;
 };
 
 const Messages: FC<Props> = ({ messages, myId }) => (
-  <div>
+  <div className={styles.messages}>
     {messages.map(({ from, message }, index) => (
       <div key={index}>
         <div>
